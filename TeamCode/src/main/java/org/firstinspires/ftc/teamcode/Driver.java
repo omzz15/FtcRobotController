3a73695de2ba1aec78fe4e3025bf9fa75fbf750f
om <drompatel5@gmail.com>
2021-11-10 11:28:41 -0500

    added Movement class and added all parts the the Driver class

--- TeamCode/src/main/java/org/firstinspires/ftc/teamcode/Driver.java
+++ TeamCode/src/main/java/org/firstinspires/ftc/teamcode/Driver.java
@@ -1,24 +1,31 @@
 package org.firstinspires.ftc.teamcode;
 
+import android.text.method.MovementMethod;
+
 import com.qualcomm.robotcore.eventloop.opmode.LinearOpMode;
 import com.qualcomm.robotcore.eventloop.opmode.TeleOp;
 
 import org.firstinspires.ftc.teamcode.base.Robot;
 import org.firstinspires.ftc.teamcode.parts.arm.Arm;
 import org.firstinspires.ftc.teamcode.parts.drive.Drive;
+import org.firstinspires.ftc.teamcode.parts.duckspinner.DuckSpinner;
 import org.firstinspires.ftc.teamcode.parts.intake.Intake;
+import org.firstinspires.ftc.teamcode.parts.movement.Movement;
 import org.firstinspires.ftc.teamcode.parts.positiontracker.PositionTracker;
+import org.firstinspires.ftc.teamcode.parts.vision.Vision;
 
 @TeleOp(name = "test", group = "Test")
 public class Driver extends LinearOpMode {
     @Override
     public void runOpMode(){
         Robot robot = new Robot(this);
-        new Drive(robot);
         new Arm(robot);
+        new Drive(robot);
+        new DuckSpinner(robot);
         new Intake(robot);
+        new Movement(robot);
         new PositionTracker(robot);
-
+        new Vision(robot);
 
         robot.init();
 
--- TeamCode/src/main/java/org/firstinspires/ftc/teamcode/parts/movement/MoveToPosSettings.java
+++ TeamCode/src/main/java/org/firstinspires/ftc/teamcode/parts/movement/MoveToPosSettings.java
@@ -1,4 +1,48 @@
 package org.firstinspires.ftc.teamcode.parts.movement;
 
-public class MoveToPosSettings {
+import com.qualcomm.robotcore.hardware.PIDCoefficients;
+
+class MoveToPosSettings
+{
+	public double[] tol;
+	public int timesInTol;
+	public int maxRuntime;
+	public double maxPower;
+	public PIDCoefficients turnPID = null;
+	public PIDCoefficients xPID = null;
+	public PIDCoefficients yPID = null;
+
+	MoveToPosSettings(){}
+	MoveToPosSettings(double[] tol, int timesInTol, int maxRuntime, double maxPower)
+	{
+		this.tol = tol;
+		this.timesInTol = timesInTol;
+		this.maxRuntime = maxRuntime;
+		this.maxPower = maxPower;
+	}
+
+	MoveToPosSettings(double[] tol, int timesInTol, int maxRuntime, double maxPower, PIDCoefficients xPID, PIDCoefficients yPID, PIDCoefficients turnPID)
+	{
+		this.tol = tol;
+		this.timesInTol = timesInTol;
+		this.maxRuntime = maxRuntime;
+		this.maxPower = maxPower;
+		this.xPID = xPID;
+		this.yPID = yPID;
+		this.turnPID = turnPID;
+	}
+
+	public boolean isPIDValid()
+	{
+		return turnPID != null && xPID != null && yPID != null;
+	}
+
+
+	TurnToAngSettings toRotAngleSettings()
+	{
+		if(turnPID != null) return new TurnToAngSettings(tol[2], timesInTol, maxRuntime, maxPower, turnPID);
+		return new TurnToAngSettings(tol[2], timesInTol, maxRuntime, maxPower);
+	}
+
+
 }
