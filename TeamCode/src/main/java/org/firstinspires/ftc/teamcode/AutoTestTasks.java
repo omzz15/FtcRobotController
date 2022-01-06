f3067ea78da578a4fae6142be0e14c267f8d6b91
Tom Kroon <soccer@kroon.ws>
2022-01-06 11:10:22 -0500

    Adi and Anna changes from Wed night

--- TeamCode/src/main/java/org/firstinspires/ftc/teamcode/AutoTestTasks.java
+++ TeamCode/src/main/java/org/firstinspires/ftc/teamcode/AutoTestTasks.java
@@ -39,12 +39,13 @@ public class AutoTestTasks extends LinearOpMode {
       // addTask("arm to dump pos", () -> arm.setArmToPreset((short) 2), );
       //  addTask("fireBG arm to dump pos", () -> robot.taskManager.getMain().getBackgroundTask("arm to dump pos"))
 
-        addTask("dump high", () -> arm.setToAPresetPosition((short)6), () -> {return true;});
-        addTask("BDump", new Position(1.6, 43, 60));
-        addDelay(15000);
-        //addTask("Cradle", () -> arm.setToAPresetPosition((short)4));
-        addTask("high dump", new Position(-4.5, 41 , 72));
+        addTask("dump low", () -> arm.setToAPresetPosition((short)6), () -> {return true;});
+        //addTask("MDump", new Position(-0.2, 47, 72));
+        addTask("BDump", new Position(4.6, 44.5, 57.5));
 
+        addDelay(5000);
+        //addTask("Cradle", () -> arm.setToAPresetPosition((short)4));
+      //  addTask("high dump", new Position(-4.5, 41 , 72));
        // addTask("dump high", () -> arm.setToAPresetPosition((short)2));
         addDelay(1000);
         addTask("cradle", () -> arm.setToAPresetPosition((short)4));
